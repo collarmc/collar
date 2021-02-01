405179ca4466355918a0e1e855e4277923addc0e
orsond <orsondmc@gmail.com>
2021-02-01 14:30:43 +1100

    upgrade handlebars as something is funky with old dependencies

--- server/src/main/java/team/catgirl/collar/server/http/HandlebarsTemplateEngine.java
+++ server/src/main/java/team/catgirl/collar/server/http/HandlebarsTemplateEngine.java
@@ -2,18 +2,13 @@ package team.catgirl.collar.server.http;
 
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
-import com.github.jknack.handlebars.cache.GuavaTemplateCache;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
-import com.github.jknack.handlebars.io.TemplateSource;
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import org.eclipse.jetty.io.RuntimeIOException;
 import spark.ModelAndView;
 import spark.TemplateEngine;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class HandlebarsTemplateEngine extends TemplateEngine {
 
@@ -24,20 +19,12 @@ public class HandlebarsTemplateEngine extends TemplateEngine {
      *
      * @param resourceRoot the resource root
      */
-    public HandlebarsTemplateEngine(String resourceRoot, boolean cacheTemplates) {
+    public HandlebarsTemplateEngine(String resourceRoot) {
         TemplateLoader templateLoader = new ClassPathTemplateLoader();
         templateLoader.setPrefix(resourceRoot);
         templateLoader.setSuffix(".mustache");
 
-        Handlebars handlebars = new Handlebars(templateLoader);
-        if (cacheTemplates) {
-            Cache<TemplateSource, Template> cache = CacheBuilder.newBuilder()
-                    .expireAfterWrite(10, TimeUnit.MINUTES)
-                    .maximumSize(1000).build();
-            this.handlebars = handlebars.with(new GuavaTemplateCache(cache));
-        } else {
-            this.handlebars = handlebars;
-        }
+        handlebars = new Handlebars(templateLoader);
     }
 
     @Override

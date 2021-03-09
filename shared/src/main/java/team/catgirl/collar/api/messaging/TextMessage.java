2d5c266dd4e2afab8557b3c4662c7a58cf65b422
orsond <orsondmc@gmail.com>
2021-03-09 17:30:01 +1100

    TextMessage now has a non-serializable consoleMessage field that is printed for the sender immediately after delivery

--- shared/src/main/java/team/catgirl/collar/api/messaging/TextMessage.java
+++ shared/src/main/java/team/catgirl/collar/api/messaging/TextMessage.java
@@ -1,5 +1,6 @@
 package team.catgirl.collar.api.messaging;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
@@ -9,14 +10,30 @@ public final class TextMessage implements Message {
 
     public static final int MAX_LENGTH = 2000;
 
+    /**
+     * The message that is sent to the recipient
+     */
     @JsonProperty("content")
     public final String content;
 
+    /**
+     * The message that is printed for the sender immediately after delivery
+     */
+    @JsonProperty("content")
+    public final String consoleMessage;
+
+    @JsonCreator
     public TextMessage(@JsonProperty("content") String content) {
         if (content.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("content exceeds " + MAX_LENGTH + " characters");
         }
         this.content = content;
+        this.consoleMessage = null;
+    }
+
+    public TextMessage(String content, String consoleMessage) {
+        this.content = content;
+        this.consoleMessage = consoleMessage;
     }
 
     @Override

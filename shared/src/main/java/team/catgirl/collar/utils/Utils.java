51dba3fcf792527a053971ef4b722f4c0b6e1d99
orsond <orsondmc@gmail.com>
2021-03-15 15:10:25 +1100

    Dirty hack to get around LetsEncrypt SSL certificate issues. Not for production.

--- shared/src/main/java/team/catgirl/collar/utils/Utils.java
+++ shared/src/main/java/team/catgirl/collar/utils/Utils.java
@@ -8,15 +8,18 @@ import org.msgpack.jackson.dataformat.MessagePackFactory;
 import team.catgirl.collar.api.session.Player;
 import team.catgirl.collar.security.mojang.MinecraftPlayer;
 
+import javax.net.ssl.*;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.StringTokenizer;
 import java.util.UUID;
 
 public final class Utils {
 
-    private static final OkHttpClient http = new OkHttpClient();
+    private static final OkHttpClient http;
     private static final ObjectMapper JSON_MAPPER;
     private static final ObjectMapper MESSAGE_PACK_MAPPER;
 
@@ -30,6 +33,8 @@ public final class Utils {
                 .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        http = getUnsafeOkHttpClient();
     }
 
     public static final SecureRandom SECURERANDOM;
@@ -55,6 +60,49 @@ public final class Utils {
     }
 
     public static OkHttpClient http() { return http; }
+
+    // TODO: figure out how to import a SSL certificate here
+    private static OkHttpClient getUnsafeOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain,
+                                                       String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain,
+                                                       String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            return new OkHttpClient.Builder()
+                    .sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0])
+                    .hostnameVerifier(new HostnameVerifier() {
+                        @Override
+                        public boolean verify(String hostname, SSLSession session) {
+                            return true;
+                        }
+                    }).build();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private Utils() {}
 }

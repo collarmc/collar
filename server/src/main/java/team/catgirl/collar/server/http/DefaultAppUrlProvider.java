c006463c95525ef015c78a6dcf4ec282e305240e
orsond <orsondmc@gmail.com>
2021-05-13 17:46:18 +1000

    correct path for local dev

--- server/src/main/java/team/catgirl/collar/server/http/DefaultAppUrlProvider.java
+++ server/src/main/java/team/catgirl/collar/server/http/DefaultAppUrlProvider.java
@@ -11,41 +11,41 @@ public class DefaultAppUrlProvider implements AppUrlProvider {
 
     @Override
     public String homeUrl() {
-        return this.baseUrl.withPath("/app").toString();
+        return this.baseUrl.withPath("/").toString();
     }
 
     @Override
     public String loginUrl() {
-        return this.baseUrl.withPath("/app/login").toString();
+        return this.baseUrl.withPath("/login").toString();
     }
 
     @Override
     public String signupUrl() {
-        return this.baseUrl.withPath("/app/signup").toString();
+        return this.baseUrl.withPath("/signup").toString();
     }
 
     @Override
     public String resetPrivateIdentity() {
-        return this.baseUrl.withPath("/app/reset/").toString();
+        return this.baseUrl.withPath("/reset/").toString();
     }
 
     @Override
     public String deviceVerificationUrl(String token) {
-        return this.baseUrl.withPath("/app/devices/trust/" + token).toString();
+        return this.baseUrl.withPath("/devices/trust/" + token).toString();
     }
 
     @Override
     public String emailVerificationUrl(String token) {
-        return this.baseUrl.withPath("/app/verify/").addParameter("token", token).toString();
+        return this.baseUrl.withPath("/verify/").addParameter("token", token).toString();
     }
 
     @Override
     public String logoUrl(int size) {
-        return this.baseUrl.withPath("/app/static/logo.png").toString();
+        return this.baseUrl.withPath("/static/logo.png").toString();
     }
 
     @Override
     public String resetPassword(String token) {
-        return this.baseUrl.withPath("/app/reset").toString();
+        return this.baseUrl.withPath("/reset").toString();
     }
 }

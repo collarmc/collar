bc20fcc687d069b2bd00bc0931e609bac7408c8c
orsond <orsondmc@gmail.com>
2021-05-14 15:46:51 +1000

    fix paths

--- server/src/main/java/team/catgirl/collar/server/http/DefaultAppUrlProvider.java
+++ server/src/main/java/team/catgirl/collar/server/http/DefaultAppUrlProvider.java
@@ -2,6 +2,9 @@ package team.catgirl.collar.server.http;
 
 import io.mikael.urlbuilder.UrlBuilder;
 
+/**
+ * Provides the default URLs for the collar web app
+ */
 public class DefaultAppUrlProvider implements AppUrlProvider {
     public final UrlBuilder baseUrl;
 
@@ -31,12 +34,12 @@ public class DefaultAppUrlProvider implements AppUrlProvider {
 
     @Override
     public String deviceVerificationUrl(String token) {
-        return this.baseUrl.withPath("/devices/trust/" + token).toString();
+        return this.baseUrl.withPath("/device/accept/" + token).toString();
     }
 
     @Override
     public String emailVerificationUrl(String token) {
-        return this.baseUrl.withPath("/verify/").addParameter("token", token).toString();
+        return this.baseUrl.withPath("/verify/" + token).toString();
     }
 
     @Override

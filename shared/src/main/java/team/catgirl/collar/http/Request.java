efa0debd38bee30a4d81e228e2a724283a0e4e0c
orsond <orsondmc@gmail.com>
2021-06-14 10:59:47 +1000

    add logging

--- shared/src/main/java/team/catgirl/collar/http/Request.java
+++ shared/src/main/java/team/catgirl/collar/http/Request.java
@@ -10,6 +10,7 @@ import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.multipart.HttpPostRequestEncoder;
 import io.netty.handler.codec.http.multipart.HttpPostRequestEncoder.ErrorDataEncoderException;
+import sun.rmi.runtime.Log;
 import team.catgirl.collar.utils.Utils;
 
 import java.net.URI;
@@ -17,11 +18,15 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * HTTP request
  */
 public final class Request {
+
+    private static final Logger LOGGER = Logger.getLogger(Request.class.getName());
 
     /**
      * Method of the request
@@ -87,6 +92,7 @@ public final class Request {
             form.forEach((name, value) -> {
                 try {
                     encoder.addBodyAttribute(name, value);
+                    LOGGER.log(Level.INFO, String.format("Form param %s:%s", name, value));
                 } catch (ErrorDataEncoderException e) {
                     throw new IllegalArgumentException(e);
                 }

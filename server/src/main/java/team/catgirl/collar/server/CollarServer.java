d81ab53a90ac9d8706576dab41886ebbae302098
orsond <orsondmc@gmail.com>
2021-07-05 20:41:01 +1000

    move back to 16 byte random token

--- server/src/main/java/team/catgirl/collar/server/CollarServer.java
+++ server/src/main/java/team/catgirl/collar/server/CollarServer.java
@@ -1,6 +1,5 @@
 package team.catgirl.collar.server;
 
-import com.google.common.hash.Hashing;
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Bucket4j;
@@ -133,7 +132,7 @@ public class CollarServer {
                     profileCache.getById(req.identity.id()).ifPresentOrElse(profile -> {
                         if (processPrivateIdentityToken(profile, request)) {
                             LOGGER.log(Level.FINE, "Profile found for " + req.identity.id());
-                            sendPlain(session, new IdentifyResponse(serverIdentity, profile.toPublic(), Mojang.serverPublicKey(), Hashing.sha1().hashBytes(TokenGenerator.byteToken(16)).asBytes()));
+                            sendPlain(session, new IdentifyResponse(serverIdentity, profile.toPublic(), Mojang.serverPublicKey(), TokenGenerator.byteToken(16)));
                         } else {
                             sendPlain(session, new PrivateIdentityMismatchResponse(serverIdentity, services.urlProvider.resetPrivateIdentity()));
                         }

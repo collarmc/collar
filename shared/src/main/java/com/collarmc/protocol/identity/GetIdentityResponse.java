905f2507d382caf57badce2f1ec657142a80c192
orsond <orsondmc@gmail.com>
2021-07-19 16:47:37 +1000

    FIX: GetIdentityResponse should include profile that was being looked up

--- shared/src/main/java/com/collarmc/protocol/identity/GetIdentityResponse.java
+++ shared/src/main/java/com/collarmc/protocol/identity/GetIdentityResponse.java
@@ -5,6 +5,8 @@ import com.collarmc.protocol.ProtocolResponse;
 import com.collarmc.security.ClientIdentity;
 import com.collarmc.security.ServerIdentity;
 
+import java.util.UUID;
+
 public final class GetIdentityResponse extends ProtocolResponse {
 
     /**
@@ -17,11 +19,16 @@ public final class GetIdentityResponse extends ProtocolResponse {
      */
     public final ClientIdentity found;
 
+    @JsonProperty("player")
+    public final UUID player;
+
     public GetIdentityResponse(@JsonProperty("identity") ServerIdentity identity,
                                @JsonProperty("id") Long id,
-                               @JsonProperty("found") ClientIdentity found) {
+                               @JsonProperty("found") ClientIdentity found,
+                               @JsonProperty("player") UUID player) {
         super(identity);
         this.id = id;
         this.found = found;
+        this.player = player;
     }
 }

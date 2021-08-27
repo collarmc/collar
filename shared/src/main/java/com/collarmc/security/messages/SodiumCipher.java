739ad02a26cd382e3dbb6f5fb14a96897d07b61d
orsond <orsondmc@gmail.com>
2021-08-27 17:59:03 +1000

    Use libsodium instead of bc for encryption, authentication and verification.
    BC on minecraft 1.12.x is just too old.

---
+++ shared/src/main/java/com/collarmc/security/messages/SodiumCipher.java
@@ -0,0 +1,83 @@
+package com.collarmc.security.messages;
+
+import com.collarmc.api.identity.Identity;
+import com.collarmc.security.PublicKey;
+import com.goterl.lazysodium.LazySodiumJava;
+import com.goterl.lazysodium.SodiumJava;
+import com.goterl.lazysodium.exceptions.SodiumException;
+import com.goterl.lazysodium.interfaces.Box;
+import com.goterl.lazysodium.interfaces.SecretBox;
+import com.goterl.lazysodium.interfaces.Sign;
+import com.goterl.lazysodium.utils.KeyPair;
+import org.bouncycastle.util.encoders.Hex;
+
+public final class SodiumCipher implements Cipher {
+
+    private static final LazySodiumJava SODIUM = new LazySodiumJava(new SodiumJava());
+
+    private final KeyPair keyPair;
+
+    public SodiumCipher(KeyPair keyPair) {
+        this.keyPair = keyPair;
+    }
+
+    @Override
+    public byte[] decrypt(byte[] content, PublicKey publicKey) throws CipherException {
+        return decrypt(content, publicKey.key);
+    }
+
+    @Override
+    public byte[] decrypt(byte[] content, Identity sender) throws CipherException {
+        return decrypt(content, sender.publicKey().key);
+    }
+
+    @Override
+    public byte[] decrypt(byte[] bytes) throws CipherException {
+        return decrypt(bytes, keyPair.getPublicKey().getAsBytes());
+    }
+
+    @Override
+    public byte[] encrypt(byte[] plain, Identity recipient) throws CipherException {
+        return encrypt(plain, recipient.publicKey().key);
+    }
+
+    @Override
+    public byte[] encrypt(byte[] plain) throws CipherException {
+        return encrypt(plain, keyPair.getPublicKey().getAsBytes());
+    }
+
+    private byte[] encrypt(byte[] plain, byte[] recipient) throws CipherException {
+        byte[] sig = new byte[Sign.BYTES + plain.length];
+        if (!SODIUM.cryptoSign(sig, plain, plain.length, keyPair.getSecretKey().getAsBytes())) {
+            throw new CipherException("Could not sign message.");
+        }
+        SignedMessage signedMessage = new SignedMessage(sig, plain);
+        byte[] signedMessageBytes = signedMessage.serialize();
+        byte[] cipherTextBytes = new byte[Box.SEALBYTES + signedMessageBytes.length];
+
+        if (!SODIUM.cryptoBoxSeal(cipherTextBytes, signedMessageBytes, signedMessageBytes.length, recipient)) {
+            throw new CipherException("Could not encrypt message.");
+        }
+        return cipherTextBytes;
+    }
+
+    private byte[] decrypt(byte[] message, byte[] sender) throws CipherException {
+        byte[] messageBytes = new byte[message.length - Box.SEALBYTES];
+        if (!SODIUM.cryptoBoxSealOpen(messageBytes, message, message.length, keyPair.getPublicKey().getAsBytes(), keyPair.getSecretKey().getAsBytes())) {
+            throw new CipherException("Could not decrypt signedMessage.");
+        }
+        SignedMessage signedMessage = new SignedMessage(messageBytes);
+        if (SODIUM.cryptoSignOpen(signedMessage.signature, signedMessage.contents, signedMessage.contents.length, sender)) {
+            throw new CipherException("Could not verify signedMessage.");
+        }
+        return signedMessage.contents;
+    }
+
+    public static KeyPair generateKeyPair() throws CipherException {
+        try {
+            return SODIUM.cryptoBoxKeypair();
+        } catch (SodiumException e) {
+            throw new CipherException("could not generate key pair", e);
+        }
+    }
+}
--- shared/src/test/java/com/collarmc/security/messages/CipherTest.java
+++ shared/src/test/java/com/collarmc/security/messages/CipherTest.java
@@ -5,15 +5,29 @@ import com.collarmc.security.TokenGenerator;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 import static org.junit.Assert.fail;
 
 public class CipherTest {
+
+    @Test
+    public void roundTrip() throws Exception {
+        CollarIdentity clientIdentity = CollarIdentity.createClientIdentity(UUID.randomUUID(), CollarIdentity.createServerIdentity().serverIdentity);
+        Cipher cipher = new SodiumCipher(clientIdentity.keyPair);
+
+        byte[] bytes = cipher.encrypt("hello world".getBytes(StandardCharsets.UTF_8));
+
+        // Try to decrypt
+        byte[] plainText = cipher.decrypt(bytes, clientIdentity.publicKey());
+        Assert.assertEquals("hello world", new String(plainText, StandardCharsets.UTF_8));
+    }
+
     @Test
     public void encryptDecryptAndMutate() throws Exception {
         CollarIdentity clientIdentity = CollarIdentity.createClientIdentity(UUID.randomUUID(), CollarIdentity.createServerIdentity().serverIdentity);
-        Cipher cipher = new Cipher(clientIdentity);
+        Cipher cipher = new SodiumCipher(clientIdentity.keyPair);
         byte[] token = TokenGenerator.byteToken(1024);
         byte[] bytes = cipher.encrypt(token);
 
